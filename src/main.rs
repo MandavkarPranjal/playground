ba9c018e88a169c72d2e37a64e516b864aaf5abf
MandavkarPranjal <pranjalmandavkar10@gmail.com>
2024-04-30 15:48:21 +0530

    basic program on how error is handled in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+fn error_me(throw: bool) -> Result<(), usize> {
+    if throw {
+        return Err(7);
+    }
+
+    return Ok(());
+}
+
+fn main() -> Result<(), usize> {
+    let value = error_me(false)?;
+
+    if error_me(true).is_ok() {}
+
+    return Ok(());
 }
